1ec5a4821de3b80bc6cfd16132a1516489b5bf57
Simon <simon@extension-interactive.com>
2018-03-29 08:30:46 +0200

    facebook plugin

--- www/app/resources/User.js
+++ www/app/resources/User.js
@@ -1,9 +1,13 @@
 'use strict';
 betty2App.factory('UserApi', function($rootScope, BtMessages, BtNavigate, BtLoading, ResourcesFactory, BtLocalStorage) {
   var UserApi = {
+
+    //User Sign In
     sign : function(newUser){
       BtLoading.startLoad();
       ResourcesFactory.post('/api/users/signin', newUser).then(function () {
+
+        //Success Sign In
         var messages = [
           {
             context:'success',
@@ -15,11 +19,14 @@ betty2App.factory('UserApi', function($rootScope, BtMessages, BtNavigate, BtLoad
           BtLoading.endLoad();
         });
       }, function (messages) {
+
+        //Sign Fail
         BtMessages.show(messages);
         BtLoading.endLoad();
         return;
       });
     },
+
     login : function(user){
       BtLoading.startLoad();
       user.logfromfb = false;
@@ -45,6 +52,32 @@ betty2App.factory('UserApi', function($rootScope, BtMessages, BtNavigate, BtLoad
         return;
       });
     },
+    fbLogin: function () {
+      return $q(function (resolve, reject) {
+        $cordovaFacebook.login(['public_profile', 'user_friends', 'email']).then(function (response) {
+          var authtoken = response.authResponse.accessToken;
+          $cordovaFacebook.api('me/' + '?fields=id, name,email,first_name,last_name,gender,picture.height(' + Config.CONST.AVATAR_HEIGHT + ').width(' + Config.CONST.AVATAR_WIDTH + ')' + '&access_token=' + authtoken, ['public_profile', 'user_friends', 'email']).then(function (response) {
+            $log.log(response);
+            var fbCredentials = {
+              logfromfb: true,
+              fbresult: response
+            };
+            User.login(fbCredentials).then(function (response) {
+              resolve(response);
+            }, function (error) {
+              reject(error);
+            });
+          }, function (error) {
+            $log.log(error);
+            reject(error);
+          });
+        }, function (error) {
+          $log.log(error);
+          reject(error);
+        });
+      });
+    },
+
   };
   return UserApi;
 });
