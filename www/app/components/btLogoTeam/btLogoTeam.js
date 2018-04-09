4cd95635aa2c3f586be600ae75831c94c7b75d33
Simon <simon@extension-interactive.com>
2018-04-09 13:47:12 +0200

    logo component

---
+++ www/app/components/btLogoTeam/btLogoTeam.js
@@ -0,0 +1,11 @@
+/**
+ * Created by simon on 24/03/18.
+ */
+betty2App.component('btLogoTeam',{
+  templateUrl:'app/components/btLogoTeam/btLogoTeam.html',
+  bindings:{
+    btTeam: '<',
+  },
+  controller:function($timeout){
+  }
+});
