9aeace4651c54a9a1ee476ed80168bb48aa72de5
Simon <simon@extension-interactive.com>
2018-05-24 13:59:42 +0200

    ranking page

--- www/app/pages/ranking/ranking.js
+++ www/app/pages/ranking/ranking.js
@@ -37,6 +37,86 @@ betty2App.controller('RankingCtrl', function (BettyLeagueApi, UserApi, ranking, 
 				BtLoading.endLoad();
 			})
 		});
+	}
+
+	rankingCtrl.canIPrev = function (ranking, next) {
+		return true;
+		switch (ranking.period) {
+			case 'full_season':
+				if (!next) {
+					if (
+						ranking.seasonNb == 1
+					) {
+						return false;
+					}
+					return true;
+				} else {
+					if (
+						ranking.seasonNb == ranking.currentRoundNbs.seasonNb
+					) {
+						return false;
+					}
+					return true;
+				}
+				break;
+			case 'half_season':
+				if (!next) {
+					if (
+						(ranking.seasonNb == 1 && ranking.halfSeasonNb == 1)
+					) {
+						return false;
+					}
+					return true;
+				} else {
+					if (
+						ranking.seasonNb == ranking.currentRoundNbs.seasonNb &&
+						ranking.halfSeasonNb == ranking.currentRoundNbs.halfSeasonNb
+					) {
+						return false;
+					}
+					return true;
+				}
+				break;
+			case 'for_rounds':
+				if (!next) {
+					if (
+						ranking.seasonNb == 1 &&
+						ranking.fourRoundsNb == 1
+					) {
+						return false;
+					}
+					return true;
+				} else {
+					if (
+						ranking.seasonNb == ranking.currentRoundNbs.seasonNb &&
+						ranking.fourRoundsNb == ranking.currentRoundNbs.fourRoundsNb
+					) {
+						return false;
+					}
+					return true;
+				}
+				break;
+			case 'round':
+				if (!next) {
+					if (
+						ranking.seasonNb == 1 &&
+						ranking.roundNb == 1
+					) {
+						return false;
+					}
+					return true;
+				} else {
+					if (
+						ranking.seasonNb == ranking.currentRoundNbs.seasonNb &&
+						ranking.roundNb == ranking.currentRoundNbs.roundNb
+					) {
+						return false;
+					}
+					return true;
+				}
+				break;
+				break;
+		}
 	};
 
 	rankingCtrl.previousPeriod = function (ranking, next) {
@@ -52,27 +132,115 @@ betty2App.controller('RankingCtrl', function (BettyLeagueApi, UserApi, ranking, 
 				};
 				break;
 			case 'half_season':
+
+				var halfSeasonNb = null;
+				if (!next) {
+					var halfSeasonNbTmp =  parseInt(ranking.halfSeasonNb) + iteration;
+				} else {
+					var halfSeasonNbTmp =  parseInt(ranking.halfSeasonNb);
+				}
+
+				var seasonNb =  parseInt(ranking.seasonNb);
+
+				if (halfSeasonNbTmp % 2 == 0) {
+					seasonNb = seasonNb + iteration;
+					if (next) {
+						halfSeasonNb = 1;
+					} else {
+						halfSeasonNb = 2;
+					}
+				}
+
+				if (next && !halfSeasonNb) {
+					halfSeasonNb =  halfSeasonNbTmp + iteration;
+				} else if (!halfSeasonNb) {
+					halfSeasonNb =  halfSeasonNbTmp;
+				}
+
 				var params = {
 					'period': ranking.period,
-					'seasonNb': ranking.seasonNb,
-					'halfSeasonNb': parseInt(ranking.halfSeasonNb) + iteration
+					'seasonNb': seasonNb,
+					'halfSeasonNb': halfSeasonNb
 				};
 				break;
 			case 'for_rounds':
+				//special cases
+				var fourRoundsNb = null;
+				if (!next) {
+					var fourRoundsNbTmp =  parseInt(ranking.fourRoundsNb) + iteration;
+				} else {
+					var fourRoundsNbTmp =  parseInt(ranking.fourRoundsNb);
+				}
+
+				var seasonNb =  parseInt(ranking.seasonNb);
+
+				if (fourRoundsNbTmp % 13 == 0) {
+					seasonNb = seasonNb + iteration;
+					if (next) {
+						fourRoundsNb = 1;
+					} else {
+						fourRoundsNb = 13;
+					}
+				}
+
+				if (next && !fourRoundsNb) {
+					fourRoundsNb =  fourRoundsNbTmp + iteration;
+				} else if (!fourRoundsNb) {
+					fourRoundsNb =  fourRoundsNbTmp;
+				}
+
 				var params = {
 					'period': ranking.period,
-					'seasonNb': ranking.seasonNb,
-					'halfSeasonNb': ranking.halfSeasonNb,
-					'fourRoundsNb': parseInt(ranking.fourRoundsNb) + iteration
+					'seasonNb': seasonNb,
+					'fourRoundsNb': fourRoundsNb
 				};
 				break;
 			case 'round':
+				//special cases
+				var roundNb = null;
+
+				if (!next) {
+					var roundNbTmp =  parseInt(ranking.roundNb) + iteration;
+				} else {
+					var roundNbTmp =  parseInt(ranking.roundNb);
+				}
+
+				var fourRoundsNb =  parseInt(ranking.fourRoundsNb);
+				var halfSeasonNb =  parseInt(ranking.halfSeasonNb);
+				var seasonNb =  parseInt(ranking.seasonNb);
+				if (roundNbTmp % 4 == 0) {
+					fourRoundsNb = fourRoundsNb + iteration;
+
+				}
+				if (roundNbTmp % 26 == 0) {
+					halfSeasonNb = halfSeasonNb + iteration;
+				}
+				if (roundNbTmp % 52 == 0) {
+					seasonNb = seasonNb + iteration;
+					if (next) {
+						roundNb = 1;
+						halfSeasonNb = 1;
+						fourRoundsNb = 1;
+
+					} else {
+						roundNb = 52;
+						halfSeasonNb = 2;
+						fourRoundsNb = 13;
+					}
+				}
+
+				if (next && !roundNb) {
+					roundNb =  roundNbTmp + iteration;
+				} else if (!roundNb) {
+					roundNb =  roundNbTmp;
+				}
+
 				var params = {
 					'period': ranking.period,
-					'seasonNb': ranking.seasonNb,
-					'halfSeasonNb': ranking.halfSeasonNb,
-					'fourRoundsNb': ranking.fourRoundsNb,
-					'roundNb': parseInt(ranking.roundNb) + iteration
+					'seasonNb': seasonNb,
+					'halfSeasonNb': halfSeasonNb,
+					'fourRoundsNb': fourRoundsNb,
+					'roundNb': roundNb
 				};
 				break;
 		}
