4a483e18f0f7b075704ced258c0433bde22f2be4
Simon Janel <simon.janel@boursorama.fr>
2022-05-08 00:45:58 +0200

    gitignore

--- www/js/config/parameters.dist.js
+++ www/js/config/parameters.dist.js
@@ -1,7 +1,8 @@
-//'use strict';
-//betty2App
-//    .constant('API_BASE_URL', "http://192.168.0.15:80/projets/betty2/web/app_dev.php")
-//    .constant('AVATAR_HEIGHT', 100)
-//    .constant('AVATAR_WIDTH', 100)
-//	.constant('ENV', 'PROD') //DEV - VIEW - PROD
-//;
+'use strict';
+betty2App
+  .constant('API_BASE_URL', "http://api.pouetpouet.tk")
+  .constant('AVATAR_HEIGHT', 100)
+  .constant('AVATAR_WIDTH', 100)
+  .constant('ENV', 'DEV') //DEV - VIEW - PROD
+  .constant('VERSION', '?ver=0.215')
+;
