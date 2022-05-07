ee9ee18072b26d117e93de1f3f7d3b8606e66c03
Simon Janel <simon.janel@boursorama.fr>
2022-05-08 00:46:56 +0200

    remove param.js

--- www/js/config/parameters.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-betty2App
-	//.constant('API_BASE_URL', "http://localhost/projets/betty2/web/app_dev.php")
-	.constant('API_BASE_URL', "http://api.betty-league.tk")
-	.constant('AVATAR_HEIGHT', 100)
-    .constant('AVATAR_WIDTH', 100)
-	.constant('ENV', 'DEV') //DEV - VIEW - PROD
-	.constant('VERSION', '?ver=0.215')
-;
